fb92fe8ea3a15fd0e8860997bb03c8507374358d
chillrod <rodramone@live.com>
2021-09-05 12:53:11 -0300

    #Feat - Cheerio added to fallback if serp api reach limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import cheerio from "cheerio";
 import "dotenv/config";
 
 const app = express();
@@ -13,20 +14,37 @@ const apiKey = process.env.apiKey;
 app.post("/", async (request, response) => {
   const { name } = request.body;
 
-  await axios
-    .get(
-      `https://serpapi.com/search.json?&tbm=isch&engine=google&q=${name}&api_key=${apiKey}`
-    )
-    .then((res) => {
-      const { data } = res;
+  try {
+    await axios
+      .get(
+        `!!!!!!https://serpapi.com/search.json?&tbm=isch&engine=google&q=${name}&api_key=${apiKey}`
+      )
+      .then((res) => {
+        const { data } = res;
 
-      const { images_results } = data;
+        const { images_results } = data;
 
-      return response.status(200).json({
-        image: images_results[0].original,
-        alt: images_results[0].title,
+        return response.status(200).json({
+          image: images_results[0].original,
+          alt: images_results[0].title,
+        });
       });
+  } catch (err) {
+    const { data } = await axios.get(
+      `https://www.google.com/search?tbm=isch&as_q=${name}&tbs=isz:lt,islt:4mp`
+    );
+
+    const $ = cheerio.load(data);
+
+    const fallBackImageDueToSerpApiLimit = $("img.yWs4tf").attr();
+
+    const { src, alt } = fallBackImageDueToSerpApiLimit;
+
+    return response.status(200).json({
+      image: src,
+      alt: name,
     });
+  }
 });
 
 app.listen(process.env.PORT || 3333, () => console.log("Server is Running"));
