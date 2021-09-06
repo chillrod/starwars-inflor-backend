0c806db50fe582a60dbc9339e31ed44a75664777
chillrod <rodramone@live.com>
2021-09-06 00:19:40 -0300

    #Feat - Api finished

--- app.js
+++ app.js
@@ -2,39 +2,19 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import cheerio from "cheerio";
-import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const apiKey = process.env.apiKey;
-
 app.post("/", async (request, response) => {
   const { name } = request.body;
 
   try {
-    await axios
-      .get(
-        `!!!!https://serpapi.com/search.json?&tbm=isch&engine=google&q=${name}&api_key=${apiKey}`
-      )
-      .then((res) => {
-        const { data } = res;
-
-        const { images_results } = data;
-
-        return response.status(200).json({
-          image: images_results[0].original,
-          alt: images_results[0].title,
-        });
-      });
-  } catch (err) {
     const { data } = await axios.get(
       `https://www.google.com/search?tbm=isch&as_q=${name}&tbs=isz:lt,islt:24mp`
     );
-
-    console.log({ data });
 
     setTimeout(() => {
       const $ = cheerio.load(data);
@@ -48,6 +28,8 @@ app.post("/", async (request, response) => {
         alt: name,
       });
     }, 100);
+  } catch (err) {
+    return res.status(400).json({ message: err });
   }
 });
 
